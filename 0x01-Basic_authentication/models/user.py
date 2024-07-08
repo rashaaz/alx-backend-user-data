4301f46e55a6755a4cecbf0ffc403a18e31ced52
rashaaz <rashaabdelaziz111@gmail.com>
2024-07-07 22:23:24 -0700

    taskes

---
+++ 0x01-Basic_authentication/models/user.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" User model with basic authentication methods.
+"""
+import hashlib
+from models.base import Base
+
+
+class User(Base):
+    """ User class inheriting from Base
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a new User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email')
+        self._password = kwargs.get('_password')
+        self.first_name = kwargs.get('first_name')
+        self.last_name = kwargs.get('last_name')
+
+    @property
+    def password(self) -> str:
+        """ Get the hashed password.
+        """
+        return self._password
+
+    @password.setter
+    def password(self, pwd: str):
+        """ Set the password after hashing it
+        """
+        if pwd is None or type(pwd) is not str:
+            self._password = None
+        else:
+            self._password = hashlib.sha256(pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd: str) -> bool:
+        """ Validate the password.
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.password is None:
+            return False
+        pwd_e = pwd.encode()
+        return hashlib.sha256(pwd_e).hexdigest().lower() == self.password
+
+    def display_name(self) -> str:
+        """ Display the user's name
+        """
+        if self.email is None and self.first_name is None \
+                and self.last_name is None:
+            return ""
+        if self.first_name is None and self.last_name is None:
+            return "{}".format(self.email)
+        if self.last_name is None:
+            return "{}".format(self.first_name)
+        if self.first_name is None:
+            return "{}".format(self.last_name)
+        else:
+            return "{} {}".format(self.first_name, self.last_name)
